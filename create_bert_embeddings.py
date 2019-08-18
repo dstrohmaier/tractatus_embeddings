e753a2b8f164c7e4dfae6f136058f1694b22f8a3
dstrohmaier <davidstrohmaier92@gmail.com>
2019-08-18 14:47:24 +0100

    now working towards saving the embeddings in a tsv format

--- create_bert_embeddings.py
+++ create_bert_embeddings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import logging
+import csv
 
 from nltk.tokenize import sent_tokenize
 
@@ -11,7 +12,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class tractatus_embeder(object):
-    def __init__(self, path_to_csv="tractatus.csv", pretrained_name="bert-base-cased", n_layers=4):
+    def __init__(self, path_to_csv="tractatus.csv", pretrained_name="bert-base-cased", n_layers=4, start_split="train"):
         """
 
         :type n_layers: int
@@ -22,7 +23,21 @@ class tractatus_embeder(object):
         self.model = BertModel.from_pretrained(pretrained_name, config=our_config)
         self.model.eval()
 
-        self.df = pd.read_csv(path_to_csv)
+        self.all_df = pd.read_csv(path_to_csv)
+
+        self.test_df = self.all_df[self.all_df.split == "test"]
+        self.train_df = self.all_df[self.all_df.split == "train"]
+        self.val_df = self.all_df[self.all_df.split == "validation"]
+
+        self._lookup_dict = {"train": self.train_df,
+                             "val": self.val_df,
+                             "test": self.test_df}
+
+        self.set_split(start_split)
+
+    def set_split(self, split="train"):
+        self._target_split = split
+        self._target_df = self._lookup_dict[split]
 
     @staticmethod
     def prepare_statement(text):
@@ -38,16 +53,16 @@ class tractatus_embeder(object):
 
         token_embeddings = []  # already introduced here because we accumulate them over sentences
         for sent in self.prepare_statement(statement):
-            logging.debug(f"Sentence: {sent}")
+            # logging.debug(f"Sentence: {sent}")
 
             encoded_statement = self.tokenizer.encode(statement)
-            logging.debug(f"Encoded statement sentence: {encoded_statement}")
+            # logging.debug(f"Encoded statement sentence: {encoded_statement}")
 
             input_ids = torch.tensor([encoded_statement])
-            logging.debug(f"input_ids: {input_ids}")
+            # logging.debug(f"input_ids: {input_ids}")
 
             segments_ids = torch.tensor([1] * len(input_ids))
-            logging.debug(f"segment_ids: {segments_ids}")
+            # logging.debug(f"segment_ids: {segments_ids}")
 
             with torch.no_grad():
                 outputs = self.model(input_ids, token_type_ids=segments_ids)
@@ -59,10 +74,10 @@ class tractatus_embeder(object):
 
                 assert tuple(encoded_layers[0].shape) == (1, len(encoded_statement), self.model.config.hidden_size)
 
-                logging.debug(f"Number of layers: {len(encoded_layers)}")
-                logging.debug(f"Number of batches: {len(encoded_layers[0])}")
-                logging.debug(f"Number of tokens:, {len(encoded_layers[0][0])}")
-                logging.debug(f"Number of hidden units:, {len(encoded_layers[0][0][0])}")
+                # logging.debug(f"Number of layers: {len(encoded_layers)}")
+                # logging.debug(f"Number of batches: {len(encoded_layers[0])}")
+                # logging.debug(f"Number of tokens:, {len(encoded_layers[0][0])}")
+                # logging.debug(f"Number of hidden units:, {len(encoded_layers[0][0][0])}")
 
                 batch_index = 0
                 for token_index in range(len(encoded_statement)):
@@ -82,16 +97,27 @@ class tractatus_embeder(object):
             # [number_of_tokens, 768]
 
             statement_embedding = torch.mean(torch.stack(summed_last_n_layers, 1), 1)
-            logging.debug(len(summed_last_n_layers))
-            logging.debug(f"statement embedding: {statement_embedding}")
+            # logging.debug(len(summed_last_n_layers))
+            # logging.debug(f"statement embedding: {statement_embedding}")
+            # logging.debug(f"Shape of embedding: {statement_embedding.shape}")
 
             return statement_embedding
 
     def create_next_embedding(self):
-        for i, row in self.df.iterrows():
+        for _, row in self._target_df.iterrows():
             embedding = self.embed_statement(row["text"])
-            yield embedding
 
+            yield embedding, row
+
+    def write_embeddings_to_file(self, main_path="embeddings.tsv", meta_path="embeddings_titles"):
+        with open(main_path, "w", newline="") as out_file, open(meta_path, "w", newline="") as meta_file:
+            out_writer = csv.writer(out_file, split="\t")
+            meta_writer = csv.writer(meta_file, split="\t")
+
+            meta_writer.write(["statement", "depth"])
+            for embedding, row in self.create_next_embedding():
+
+        pass
 
 if __name__ == "__main__":
     te = tractatus_embeder()
--- create_csv.py
+++ create_csv.py
@@ -1,33 +1,78 @@
 import csv
-
-with open("tractatus-body.txt", "r") as infile, open("tractatus.csv", "w", newline="") as outfile:
-    fieldnames=["count", "depth", "number", "text"]
-    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-    writer.writeheader()
-
-    count = 0
-    start = True
-    for line in infile:
-        if line == "\n":
-            start = True
-            continue
-        
-        if start:
-            if count != 0:
-                writer.writerow({"count": count, "depth": depth,
-                                 "number": number, "text": text[:-1]})
-            
-            first_space_ix = line.find(" ")
-            number = line[:first_space_ix]
-            text = line[first_space_ix+1:]
-            depth = max(1, len(number)-1) # eliminates dot
-            count += 1
-            start = False
-        else:
-            text += line
-        
-
-        
-        
+import random
 
 
+def create_base_csv(source="tractatus-body.txt", destination="tractatus.csv"):
+    with open(source, "r") as infile, open(destination, "w", newline="") as outfile:
+        fieldnames = ["count", "depth", "number", "text"]
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        count = 0
+        start = True
+        for line in infile:
+            if line == "\n":
+                start = True
+                continue
+
+            if start:
+                if count != 0:
+                    writer.writerow({"count": count, "depth": depth,
+                                     "number": number, "text": text[:-1]})
+
+                first_space_ix = line.find(" ")
+                number = line[:first_space_ix]
+                text = line[first_space_ix + 1:]
+                depth = max(1, len(number) - 1)  # eliminates dot
+                count += 1
+                start = False
+            else:
+                text += line
+
+        return count
+
+
+def add_splits(source="tractatus.csv", destination="tractatus_with_splits.csv", row_count=None):
+    with open(source, "r") as infile, open(destination, "w", newline="") as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+
+        if row_count is None: # only if length of csv is unknown
+            row_count = -1 # -1 because we discount the header row
+            for row in reader:
+                row_count += 1
+            infile.seek(0)
+            reader = csv.reader(infile)
+
+
+        train_count = row_count // 100 * 80
+        test_count = row_count // 100 * 10
+        validate_count = row_count // 100 * 10
+        sum_count = train_count + test_count + validate_count
+
+        if sum_count < row_count:
+            train_count += row_count - sum_count
+        elif sum_count > row_count:
+            train_count -= sum_count - row_count
+
+        labels = ["train" for _ in range(train_count)] + \
+                 ["test" for _ in range(test_count)] + \
+                 ["validation" for _ in range(validate_count)]
+        random.shuffle(labels)
+
+        assert len(labels) == row_count, "Number of split labels and number of rows differ"
+
+        header_row = next(reader) + ["split"]
+        #print(header_row)
+
+        content_rows = []
+        for row,  label in zip(reader, labels):
+            #print(row)
+            row.append(label)
+            content_rows.append(row)
+
+        writer.writerows([header_row]+content_rows)
+
+if __name__ == "__main__":
+    row_count = create_base_csv()
+    add_splits(row_count=row_count)
